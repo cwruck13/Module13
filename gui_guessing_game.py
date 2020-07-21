2dd39e54cf1401b4795fdf5eb27219fcd0fcc0e5
cwruck13 <66025028+cwruck13@users.noreply.github.com>
2020-07-21 11:16:23 -0500

    reset buttons

--- gui_guessing_game.py
+++ gui_guessing_game.py
@@ -1,46 +1,69 @@
 import random as rand
 import tkinter
 
-#Resets games
-#Allow user to push button with their corresponding guess.
-#If correct guess
-#Create a WINNER window or messagebox to pop up
+
+
+
 #Reset game
 #Else
 #Set the button to visible but not clickable
 #Add guessed number to guessed_list in object of type NumberGuesser
 
+#functions to let user pick input
+#Allow user to push button with their corresponding guess.
 def pick_one():
-    pass
+    label.config(text="1")
 
 def pick_two():
-    #guess = int(button_2.get())
-    pass
+    label.config(text="2")
 
 def pick_three():
-    pass
+    label.config(text="3")
 
 def pick_four():
-    pass
+    label.config(text="4")
 
 def pick_five():
-    pass
+    label.config(text="5")
 
 def pick_six():
-    #guess = int(button_2.get())
-    pass
+    label.config(text="6")
 
 def pick_seven():
-    pass
+    label.config(text="7")
 
 def pick_eight():
-    pass
+    label.config(text="8")
 
 def pick_nine():
-    pass
+    label.config(text="9")
 
 def pick_ten():
-    pass
+    label.config(text="10")
+
+#Generates a random number between 1 and MAX.
+def generate_random_number():
+    global random_number
+    random_number = rand.randrange(1,10)
+    label.config(str(random_number))
+
+#If correct guess
+#Create a WINNER window or messagebox to pop up
+#def winner():
+  # winner = tkMessageBox.showinfo("WINNER", "User input won")
+
+#Resets games
+def clear_func():
+    button_1.set("")
+    button_2.set("")
+    button_3.set("")
+    button_4.set("")
+    button_5.set("")
+    button_6.set("")
+    button_7.set("")
+    button_8.set("")
+    button_9.set("")
+    button_10.set("")
 
 g = tkinter.Tk()
 
@@ -49,34 +72,28 @@ widgets are added here
 '''
 #title
 g.title('Guessing Game')
-#g.geometry('400x400')
+
+#label
 label = tkinter.Label(g, text="Waiting for user pick")
 label.grid(row=11)
 
+
 #With buttons each numbered 1 to MAX. You can select 10 or more buttons for this, deciding at design time.
-button_1= tkinter.Button(g, text='1', width=10).grid(row=1)
-button_2= tkinter.Button(g, text='2', width=10).grid(row=2)
-button_3= tkinter.Button(g, text='3', width=10).grid(row=3)
-button_4= tkinter.Button(g, text='4', width=10).grid(row=4)
-button_5= tkinter.Button(g, text='5', width=10).grid(row=5)
-button_6= tkinter.Button(g, text='6', width=10).grid(row=6)
-button_7= tkinter.Button(g, text='7', width=10).grid(row=7)
-button_8= tkinter.Button(g, text='8', width=10).grid(row=8)
-button_9= tkinter.Button(g, text='9', width=10).grid(row=9)
-button_10= tkinter.Button(g, text='10', width=10).grid(row=10)
-
-#Generates a random number between 1 and MAX.
-def generate_random_number():
-    global random_number
-    random_number = rand.randrange(1,10)
-
-
-
+button_1= tkinter.Button(g, text='1', width=10, command=pick_one).grid(row=1)
+button_2= tkinter.Button(g, text='2', width=10, command=pick_two).grid(row=2)
+button_3= tkinter.Button(g, text='3', width=10, command=pick_three).grid(row=3)
+button_4= tkinter.Button(g, text='4', width=10, command=pick_four).grid(row=4)
+button_5= tkinter.Button(g, text='5', width=10, command=pick_five).grid(row=5)
+button_6= tkinter.Button(g, text='6', width=10, command=pick_six).grid(row=6)
+button_7= tkinter.Button(g, text='7', width=10, command=pick_seven).grid(row=7)
+button_8= tkinter.Button(g, text='8', width=10, command=pick_eight).grid(row=8)
+button_9= tkinter.Button(g, text='9', width=10, command=pick_nine).grid(row=9)
+button_10= tkinter.Button(g, text='10', width=10, command=pick_ten).grid(row=10)
 
 #With a button 'Start Game'
 button_start = tkinter.Button(g, text='Start Game', width=25).grid(row=12)
 
-button_reset = tkinter.Button(g, text='Reset', width=25).grid(row=13)
+button_reset = tkinter.Button(g, text='Reset', width=25, command=clear_func).grid(row=13)
 
 g.mainloop()
 
