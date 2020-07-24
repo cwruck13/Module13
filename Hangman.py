bdb0f2ea123312cbc10e74ba2a6b800b05727b84
cwruck13 <66025028+cwruck13@users.noreply.github.com>
2020-07-24 10:18:15 -0500

    intial commit, hangman class

---
+++ Hangman.py
@@ -0,0 +1,12 @@
+"""
+Program: Hangman.py
+Author: Cassandra Wruck
+Date: 7/24/20
+
+This program will have the hangman class that will link to the master class.
+"""
+
+class Hangman:
+
+    def __int__(self, word):
+        pass
